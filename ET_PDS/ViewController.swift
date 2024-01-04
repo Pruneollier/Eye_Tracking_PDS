75fc6497c863d0d8e57b80ada56535eb3d7ea07d
Pruneollier <91121616+Pruneollier@users.noreply.github.com>
2024-01-04 23:47:20 +0100

    ViewController Commented

--- ET_PDS/ViewController.swift
+++ ET_PDS/ViewController.swift
@@ -15,7 +15,7 @@ let axesURL = Bundle.main.url(forResource: "axes", withExtension: "scn")!
 
 class ViewController: UIViewController, ARSessionDelegate {
     
-    // Material for plotting data
+    /* Material for exporting data to CSV files */
     struct PointData {
         var point: CGPoint
         var timestamp: String
@@ -44,49 +44,34 @@ class ViewController: UIViewController, ARSessionDelegate {
     var distToScreenData = [DistanceData]()
     let dateFormatter = DateFormatter()
     var t0 = String()
-
+    /*----------------------------------------------*/
     
     @IBOutlet weak var sceneView: ARSCNView!
     var overlay: SKScene!
     
-    //ARKit 3D objects
-    //ARFaceAnchor's axes (reference node for the face referential)
+    /* ARKit 3D objects */
     let faceAxes = SCNReferenceNode(url: axesURL)!
-    
-    //Gaze projection on the screen plane, in the world referential
-    let projectedNode = SCNNode(geometry: SCNSphere(radius: 0.002))
-
-    //Node which position is lookAtPoint (face referential)
-    let lookAtPointNode = SCNNode(geometry: SCNSphere(radius: 0.01))
-
-    //Reference node for the right eye referential (in reality anchored on the user's left eye)
-    let rightEyeNode = SCNReferenceNode(url: axesURL)!
-    
-    //Nodes storing the lookAtPoint's position during calibraton phase
+    let projectedNode = SCNNode(geometry: SCNSphere(radius: 0.002)) //Gaze projection on the screen plane, in the world referential
+    let lookAtPointNode = SCNNode(geometry: SCNSphere(radius: 0.01)) //Node which position is lookAtPoint (face referential)
+    let rightEyeNode = SCNReferenceNode(url: axesURL)! //Reference node for the right eye referential (in reality anchored on the user's left eye)
     let upperLeft = SCNNode(geometry: SCNSphere(radius: 0.01))
     let upperRight = SCNNode(geometry: SCNSphere(radius: 0.01))
     let downLeft = SCNNode(geometry: SCNSphere(radius: 0.01))
     
     
-    //SpriteKit 2D objects
-    //Final gaze's estimation indicator on the screen
-    var pointSKNode = SKShapeNode(circleOfRadius: 20)
-    //Referential node for calibration
-    var calibNode = SKShapeNode(circleOfRadius: 40)
-
+    /* SpriteKit 2D objects */
+    var pointSKNode = SKShapeNode(circleOfRadius: 20) //Final gaze's estimation indicator on the screen
+    var calibNode = SKShapeNode(circleOfRadius: 40) //Referential node for calibration
     //Touch node and position
     let touchedNode = SKShapeNode(circleOfRadius: 30)
     var touchPosition: CGPoint = CGPointZero
 
-    // a later stores the width of the screen in CGPoints
+
+    // variables
     var a = CGFloat()
-    // b later stores the length of the screen in CGPoints
     var b = CGFloat()
-    // length later stores the estimated length of the screen in meters
     var length = Float()
-    // width later stores the estimated width of the screen in meters
     var width = Float()
-    // pointsPerMeter later stores the estimated number of CGPoints per meter
     var pointsPerMeter = Float()
     
     //Array of CGPoints storing each frame's estimated position of the gaze
@@ -95,20 +80,23 @@ class ViewController: UIViewController, ARSessionDelegate {
     //is true when the calibration phase is over
     var run = false
     
-    //Called when the app is launched
+    /*Called when the app is launched
+     Sets the view and the scene
+     Sets the SpriteKit overlay to display 2D elements
+     Load nodes and establish hierarchy
+     Set the nodes' colors
+     Stores dimensions of the screen in CGPoints in a and b
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Set the view and the scene
         sceneView.session.delegate = self
         sceneView.scene = SCNScene()
         
-        //Set the SpriteKit overlay to display 2D elements
         overlay = SKScene(size: sceneView.bounds.size)
         overlay.backgroundColor = UIColor.clear
         sceneView.overlaySKScene = overlay
         
-        //loading nodes and establishing hierarchy
         faceAxes.load()
         rightEyeNode.load()
         sceneView.scene.rootNode.addChildNode(upperLeft)
@@ -117,7 +105,6 @@ class ViewController: UIViewController, ARSessionDelegate {
         overlay.addChild(calibNode)
         overlay.addChild(touchedNode)
         
-        //node colors
         projectedNode.isHidden = true
         pointSKNode.fillColor = .blue
         pointSKNode.strokeColor = .blue
@@ -125,85 +112,63 @@ class ViewController: UIViewController, ARSessionDelegate {
         touchedNode.strokeColor = .green
         calibNode.fillColor = .black
         
-        //The initial position of the gaze's estimator is set to the center of the screen
         pointSKNode.position = CGPoint(x: a/2, y: b/2)
 
         sceneView.overlaySKScene?.isUserInteractionEnabled = false
         
-        //a stores the width of the screen in CGPoint units
         a = sceneView.frame.width
-        //b stores the width of the screen in CGPoint units
         b = sceneView.frame.height
         
         run = false
         
-        //Material for plotting
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
         t0 = dateFormatter.string(from: Date())
     }
     
-    //Called when the app opens
+    /* Called when the app opens */
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         guard ARFaceTrackingConfiguration.isSupported else { return }
         
-        // Create a session configuration
         let configuration = ARFaceTrackingConfiguration()
-        // Set the origin of the world referential to the camera of the device
         configuration.worldAlignment = .camera
-        
-        // Run the session
         sceneView.session.run(configuration)
         
     }
     
-    // Called when the app is closed
+    /* Exports data to CSV files for plotting and pauses the session when the app closes */
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        //Export data to CSV files for plotting
         exportCSV(dataArray: ptsData, filename: "ptsData")
         exportCSV(dataArray: touchData, filename: "touchData")
         exportCSV(dataArray: eulerAnglesData, filename: "eulerData")
         exportCSV(dataArray: distToScreenData, filename: "distData")
         
-        // Pause the session
         sceneView.session.pause()
     }
     
-    // Called each time the frame is updated, ie. 60 times per seconds (ARKit's fps value)
+    /* Called each time the frame is updated, ie. 60 times per seconds (ARKit's fps value)
+    Keep the node's position updated with the value of ARKit's lookAtPoint estimation
+    Computes the gaze estimation indicator's position */
     func session(_ session: ARSession, didUpdate frame : ARFrame) {
         if let faceAnchor = frame.anchors.compactMap({
             $0 as? ARFaceAnchor
         }).first {
             
-            //Material for the CSV export
-            let formattedDate = dateFormatter.string(from: Date())
-            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-            let date1 = dateFormatter.date(from: t0)
-            let date2 = dateFormatter.date(from: formattedDate)
-            let difference = date2!.timeIntervalSince(date1!)
-            let formattedDifference = formatTimeInterval(difference)
-                
-
-            //set simdTransform of nodes
+            let formattedDifference = formatTimeInterval(dateFormatter.string(from: Date()))
+            
             faceAxes.simdTransform = faceAnchor.transform
             rightEyeNode.simdTransform = faceAnchor.rightEyeTransform
-            
-            //Keep the node's position updated with the value of ARKit's lookAtPoint estimation
             lookAtPointNode.simdPosition = faceAnchor.lookAtPoint
             
-            //Material for linePlane()
             let p0_line = simd_float3(faceAxes.worldPosition)
             let p1_line = simd_float3(lookAtPointNode.worldPosition)
             let p0_plane = simd_float3(0,0,0)
             let normal_plane = simd_float3(0,0,1)
-            
-        
             projectedNode.position = SCNVector3(linePlane(p0_line: p0_line , p1_line: p1_line, p0_plane: p0_plane, normal_plane: normal_plane))
             projectedNode.position = SCNVector3(projectedNode.position.x, projectedNode.position.y, -0.05)
-            
             
             touchedNode.position = CGPoint(x: Double(self.touchPosition.x), y: Double(self.touchPosition.y))
            
@@ -225,6 +190,9 @@ class ViewController: UIViewController, ARSessionDelegate {
         }
     }
     
+    /*
+     Sets hierarchy and starts calibration phase when a new ARFaceAnchor is added
+     */
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         if anchors.compactMap({
             $0 as? ARFaceAnchor
@@ -235,16 +203,24 @@ class ViewController: UIViewController, ARSessionDelegate {
             faceAxes.addChildNode(rightEyeNode)
             overlay.addChild(pointSKNode)
             calibration()
-
         }
     }
     
-    // Convert ARKit node's position to SpriteKit point
+    /*
+     Converts position in world referential to position in SpriteKit referential
+     worldPosition : position in the world referential (meters)
+     return : poisition in the SpriteKit referential (CGPoint)
+     */
     func convertARKitPositionToSpriteKitPoint(worldPosition: SIMD3<Float>) -> CGPoint {
         
         return CGPoint(x: a/2 + CGFloat(Float(worldPosition.y) * pointsPerMeter), y: b - CGFloat(Float(worldPosition.x) * pointsPerMeter) )
     }
     
+    /* Computes the intersection of a given line with a given plane, in the world referential
+     p0_line : first point of the line
+     p1_line : second point of the line
+     p0_plane : point in the plane
+     normal_plane : vector normal to the plane */
     func linePlane(p0_line: SIMD3<Float>, p1_line: SIMD3<Float>, p0_plane: SIMD3<Float> , normal_plane:SIMD3<Float>) -> SIMD3<Float> {
         
         var ret = simd_float3.zero
@@ -260,75 +236,65 @@ class ViewController: UIViewController, ARSessionDelegate {
             fac = -simd_dot(normal_plane, w) / dot
             ret = p0_line + (u * fac)
         }
-        //print(dot, u, w, fac)
         return ret
     }
     
+    /* Estimates the dimensions of the screen in meters.
+     length stores the estimated length in meters
+     width stores the estimated width in meters
+     pointsPerMeter stores the estimated number of CGPoints per meter
+     calibNode changes position every 4 seconds
+     upperLeft, upperRight and downLeft store the positions of the lookAtPoint at the given moment */
     func calibration(){
-        // Separate events by 2 seconds
         let delayInSeconds: TimeInterval = 2.0
 
         DispatchQueue.main.asyncAfter(deadline: .now() + delayInSeconds) {
             self.calibNode.position = (CGPoint(x:0,y:0))
             self.sceneView.unprojectPoint(SCNVector3(self.calibNode.position.x, self.calibNode.position.y, 0))
-            
         }
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + (2 * delayInSeconds)) {
             self.upperLeft.position = self.lookAtPointNode.worldPosition
-            print(self.upperLeft.worldPosition)
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + (3 * delayInSeconds)) {
-            // Code to be executed after another 2 seconds
             self.calibNode.position = (CGPoint(x:0,y:self.b))
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + (4 * delayInSeconds)) {
             self.upperRight.position = self.lookAtPointNode.worldPosition
-            print(self.upperRight.worldPosition)
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + (5 * delayInSeconds)) {
-            // Code to be executed after another 2 seconds
             self.calibNode.position = (CGPoint(x:self.a,y:0))
-            
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + (6 * delayInSeconds)) {
             self.downLeft.position = self.lookAtPointNode.worldPosition
-            print(self.downLeft.worldPosition)
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + (7 * delayInSeconds)) {
             self.length = self.upperLeft.position.x - self.upperRight.position.x
-            print(self.length)
             self.width = self.downLeft.position.y - self.upperRight.position.y
-            print(self.width)
-            self.pointsPerMeter = Float(self.b) / self.length
-            print(self.pointsPerMeter)
             self.pointsPerMeter = Float(self.a) / self.width
-            print(self.pointsPerMeter)
             self.run = true
         }
         
     }
     
-    
+    /* Returns the average position of the last k elements of array
+     array : Array of CGPoints
+     k : number of elements wanted to compute the average from 
+     */
     func averageOfLastKCGPoints(array: [CGPoint], k: Int) -> CGPoint? {
-        // Check if the array has enough elements
         guard array.count >= k else {
-            return nil // Return nil if there are not enough elements
-        }
+            return nil }
         
-        // Extract the last k elements from the array
         let lastKCGPoints = Array(array.suffix(k))
         
-        // Calculate the sum of the last k elements
         let sumX = lastKCGPoints.reduce(0) { $0 + $1.x }
         let sumY = lastKCGPoints.reduce(0) { $0 + $1.y }
         
-        
-        // Calculate the average
         let averageX = sumX / CGFloat(k)
         let averageY = sumY / CGFloat(k)
         
@@ -336,8 +302,12 @@ class ViewController: UIViewController, ARSessionDelegate {
         return averageCGPoint
     }
     
+    /*
+     Exports dataArray as a CSV file of the form "X,Y,Timestamp"
+     dataArray : Array of PointData
+     filename : wanted name for the CSV file
+     */
     func exportCSV(dataArray: [PointData], filename : String){
-        print("export called")
         var csvText = "X,Y,Timestamp\n"
         
         for data in dataArray {
@@ -345,13 +315,10 @@ class ViewController: UIViewController, ARSessionDelegate {
                 csvText.append(line)
         }
         
-        // Get the document directory URL
         if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
 
-            // Add a file name
             let fileURL = documentsDirectory.appendingPathComponent("\(filename).csv")
 
-            // Write to the file
             do {
                 try csvText.write(to: fileURL, atomically: true, encoding: .utf8)
                 print("CSV file created at \(fileURL)")
@@ -362,8 +329,12 @@ class ViewController: UIViewController, ARSessionDelegate {
         
     }
     
+    /*
+     Exports dataArray as a CSV file of the form "Timestamp,Xrot,Yrot,Zrot,Xdiff,Ydiff"
+     dataArray : Array of PointData
+     filename : wanted name for the CSV file
+     */
     func exportCSV(dataArray: [PointDiff], filename : String){
-        print("export called")
         var csvText = "Timestamp,Xrot,Yrot,Zrot,Xdiff,Ydiff\n"
         
         for data in dataArray {
@@ -371,13 +342,10 @@ class ViewController: UIViewController, ARSessionDelegate {
                 csvText.append(line)
         }
         
-        // Get the document directory URL
         if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
 
-            // Add a file name
             let fileURL = documentsDirectory.appendingPathComponent("\(filename).csv")
 
-            // Write to the file
             do {
                 try csvText.write(to: fileURL, atomically: true, encoding: .utf8)
                 print("CSV file created at \(fileURL)")
@@ -388,8 +356,12 @@ class ViewController: UIViewController, ARSessionDelegate {
         
     }
     
+    /*
+     Exports dataArray as a CSV file of the form "Timestamp, Xface, Yface, Zface, Xdiff, Ydiff"
+     dataArray : Array of PointData
+     filename : wanted name for the CSV file
+     */
     func exportCSV(dataArray: [DistanceData], filename: String){
-        print("export called")
         var csvText = "Timestamp, Xface, Yface, Zface, Xdiff, Ydiff\n"
         
         for data in dataArray {
@@ -397,13 +369,10 @@ class ViewController: UIViewController, ARSessionDelegate {
                 csvText.append(line)
         }
         
-        // Get the document directory URL
         if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
 
-            // Add a file name
             let fileURL = documentsDirectory.appendingPathComponent("\(filename).csv")
 
-            // Write to the file
             do {
                 try csvText.write(to: fileURL, atomically: true, encoding: .utf8)
                 print("CSV file created at \(fileURL)")
@@ -413,8 +382,16 @@ class ViewController: UIViewController, ARSessionDelegate {
         }
     }
    
-    // Function to format TimeInterval to "mm:ss.sss"
-    func formatTimeInterval(_ interval: TimeInterval) -> String {
+    /*
+     Formats TimeInterval to "mm:ss.sss"
+     */
+    func formatTimeInterval(_ formattedDate: String) -> String {
+        
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let date1 = dateFormatter.date(from: t0)
+        let date2 = dateFormatter.date(from: formattedDate)
+        let interval = date2!.timeIntervalSince(date1!)
+        
         let minutes = Int(interval) / 60
         let seconds = Int(interval) % 60
         let milliseconds = Int(interval.truncatingRemainder(dividingBy: 1) * 1000)
