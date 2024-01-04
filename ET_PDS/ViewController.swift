227edc4356ca0865da276e9f1f94497fe94643f6
Pruneollier <91121616+Pruneollier@users.noreply.github.com>
2024-01-04 22:53:11 +0100

    Report

--- ET_PDS/ViewController.swift
+++ ET_PDS/ViewController.swift
@@ -15,6 +15,7 @@ let axesURL = Bundle.main.url(forResource: "axes", withExtension: "scn")!
 
 class ViewController: UIViewController, ARSessionDelegate {
     
+    // Material for plotting data
     struct PointData {
         var point: CGPoint
         var timestamp: String
@@ -37,168 +38,149 @@ class ViewController: UIViewController, ARSessionDelegate {
         var Xdiff: CGFloat
         var Ydiff: CGFloat
     }
-    
     var ptsData = [PointData]()
     var touchData = [PointData]()
     var eulerAnglesData = [PointDiff]()
     var distToScreenData = [DistanceData]()
-        
+    let dateFormatter = DateFormatter()
+    var t0 = String()
+
+    
     @IBOutlet weak var sceneView: ARSCNView!
     var overlay: SKScene!
     
-    let dateFormatter = DateFormatter()
-
-    // axes fixes au milieu de l'écran
-    let axes = SCNReferenceNode(url: axesURL)!
-    
-    //axes rattachés au visage
+    //ARKit 3D objects
+    //ARFaceAnchor's axes (reference node for the face referential)
     let faceAxes = SCNReferenceNode(url: axesURL)!
     
-    //node final du regard sur l'écran (vert)
+    //Gaze projection on the screen plane, in the world referential
     let projectedNode = SCNNode(geometry: SCNSphere(radius: 0.002))
 
-    //lookAtPoint as a node (in face coordinates)
+    //Node which position is lookAtPoint (face referential)
     let lookAtPointNode = SCNNode(geometry: SCNSphere(radius: 0.01))
 
-    //node de l'oeil droit (gauche en réalité?)
+    //Reference node for the right eye referential (in reality anchored on the user's left eye)
     let rightEyeNode = SCNReferenceNode(url: axesURL)!
     
-    //node rouge pour les tests
-    let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.01))
-    
-    
-    //nodes for calibraton
+    //Nodes storing the lookAtPoint's position during calibraton phase
     let upperLeft = SCNNode(geometry: SCNSphere(radius: 0.01))
     let upperRight = SCNNode(geometry: SCNSphere(radius: 0.01))
     let downLeft = SCNNode(geometry: SCNSphere(radius: 0.01))
     
-    //node SpriteKit
+    
+    //SpriteKit 2D objects
+    //Final gaze's estimation indicator on the screen
     var pointSKNode = SKShapeNode(circleOfRadius: 20)
-    let markerNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 20, height: 20))
+    //Referential node for calibration
     var calibNode = SKShapeNode(circleOfRadius: 40)
 
-    // ADD
+    //Touch node and position
     let touchedNode = SKShapeNode(circleOfRadius: 30)
     var touchPosition: CGPoint = CGPointZero
-    
+
+    // a later stores the width of the screen in CGPoints
     var a = CGFloat()
+    // b later stores the length of the screen in CGPoints
     var b = CGFloat()
-    var PPI = CGFloat()
+    // length later stores the estimated length of the screen in meters
     var length = Float()
+    // width later stores the estimated width of the screen in meters
     var width = Float()
+    // pointsPerMeter later stores the estimated number of CGPoints per meter
     var pointsPerMeter = Float()
     
+    //Array of CGPoints storing each frame's estimated position of the gaze
     var pts = [CGPoint(x: 0.0, y: 0)]
-    var touchPositions = [CGPoint(x: 0.0, y: 0)]
+    
+    //is true when the calibration phase is over
     var run = false
-    var t0 = String()
+    
+    //Called when the app is launched
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
+        //Set the view and the scene
         sceneView.session.delegate = self
         sceneView.scene = SCNScene()
         
-        
         //Set the SpriteKit overlay to display 2D elements
-        
-        /*let skScene = SKScene(size: .zero)
-        skScene.scaleMode = .resizeFill
-        
-        sceneView.overlaySKScene = skScene*/
-        
         overlay = SKScene(size: sceneView.bounds.size)
         overlay.backgroundColor = UIColor.clear
-        markerNode.position = CGPoint(x: overlay.size.width / 2, y: overlay.size.height / 2)
-        overlay.addChild(markerNode)
         sceneView.overlaySKScene = overlay
-        overlay.addChild(calibNode)
-        calibNode.fillColor = .black
-        overlay.addChild(touchedNode)
-
         
-        //loading nodes
+        //loading nodes and establishing hierarchy
         faceAxes.load()
         rightEyeNode.load()
-        sceneView.scene.rootNode.addChildNode(sphereNode)
         sceneView.scene.rootNode.addChildNode(upperLeft)
         sceneView.scene.rootNode.addChildNode(upperRight)
         sceneView.scene.rootNode.addChildNode(downLeft)
-
-        sphereNode.position = SCNVector3(0.1,0.1,-0.5)
-        /*axes.load()
-        sceneView.scene.rootNode.addChildNode(axes)
-        axes.position = SCNVector3(0, 0, -0.05)*/
+        overlay.addChild(calibNode)
+        overlay.addChild(touchedNode)
         
         //node colors
-        projectedNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 65/255, green: 220/255, blue: 23/255, alpha: 0.8)
-        sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 220/255, green: 65/255, blue: 23/255, alpha: 0.8)
+        projectedNode.isHidden = true
         pointSKNode.fillColor = .blue
         pointSKNode.strokeColor = .blue
         touchedNode.fillColor = .green
         touchedNode.strokeColor = .green
-
-        /*print("rootNode worldTransform : \n", sceneView.scene.rootNode.worldTransform, "\n")
-        print("rootNode transform : \n", sceneView.scene.rootNode.transform, "\n")
-        print("rootNode simdTransform : \n", sceneView.scene.rootNode.simdTransform , "\n")
-        print("rootNode.orientation : \n", sceneView.scene.rootNode.orientation)*/
+        calibNode.fillColor = .black
         
-        //Pourquoi le touch est détecté alors ??
+        //The initial position of the gaze's estimator is set to the center of the screen
+        pointSKNode.position = CGPoint(x: a/2, y: b/2)
+
         sceneView.overlaySKScene?.isUserInteractionEnabled = false
         
+        //a stores the width of the screen in CGPoint units
         a = sceneView.frame.width
-        print("a:", a)
-        
+        //b stores the width of the screen in CGPoint units
         b = sceneView.frame.height
-        print("b:", b)
-        PPI = UIScreen.main.scale
-        print(PPI)
         
-        pointSKNode.position = CGPoint(x: a/2, y: b/2)
         run = false
         
+        //Material for plotting
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
         t0 = dateFormatter.string(from: Date())
-        print(t0)
     }
     
+    //Called when the app opens
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print(ARFaceTrackingConfiguration.isSupported)
         guard ARFaceTrackingConfiguration.isSupported else { return }
-
         
         // Create a session configuration
         let configuration = ARFaceTrackingConfiguration()
+        // Set the origin of the world referential to the camera of the device
         configuration.worldAlignment = .camera
         
-        // Run the view's session
+        // Run the session
         sceneView.session.run(configuration)
         
     }
     
+    // Called when the app is closed
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
+        //Export data to CSV files for plotting
         exportCSV(dataArray: ptsData, filename: "ptsData")
         exportCSV(dataArray: touchData, filename: "touchData")
         exportCSV(dataArray: eulerAnglesData, filename: "eulerData")
         exportCSV(dataArray: distToScreenData, filename: "distData")
-        // Pause the view's session
+        
+        // Pause the session
         sceneView.session.pause()
     }
     
-    
+    // Called each time the frame is updated, ie. 60 times per seconds (ARKit's fps value)
     func session(_ session: ARSession, didUpdate frame : ARFrame) {
         if let faceAnchor = frame.anchors.compactMap({
             $0 as? ARFaceAnchor
         }).first {
             
+            //Material for the CSV export
             let formattedDate = dateFormatter.string(from: Date())
-            
             dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-            
             let date1 = dateFormatter.date(from: t0)
             let date2 = dateFormatter.date(from: formattedDate)
             let difference = date2!.timeIntervalSince(date1!)
@@ -209,40 +191,22 @@ class ViewController: UIViewController, ARSessionDelegate {
             faceAxes.simdTransform = faceAnchor.transform
             rightEyeNode.simdTransform = faceAnchor.rightEyeTransform
             
-            //create a node for lookAtPoint
+            //Keep the node's position updated with the value of ARKit's lookAtPoint estimation
             lookAtPointNode.simdPosition = faceAnchor.lookAtPoint
-
-            /*print("pointOfView.worldPosition :", sceneView.pointOfView?.worldPosition)
-            print("rootNode.worldPosition : ",sceneView.scene.rootNode.worldPosition)
-            print("lookAtPointNode.worldPosition : ", lookAtPointNode.worldPosition)*/
             
-            // afficher le projectedNode si la position de lookAtPoint est estimée être à plus ou moins 10 cm de l'écran
-            /*if(lookAtPointNode.worldPosition.z > -0.1 && lookAtPointNode.worldPosition.z < 0.1){
-                print("lookAtPoint : ", lookAtPointNode.worldPosition)
-                projectedNode.position = SCNVector3(lookAtPointNode.worldPosition.x, lookAtPointNode.worldPosition.y, -0.05 )
-            }*/
-            
-            
-            //projectedNode.position = SCNVector3(lookAtPointNode.worldPosition.x , lookAtPointNode.worldPosition.y, -0.05)
-            //projectedNode.position = SCNVector3(b/2,0,0)
-            
-            
+            //Material for linePlane()
             let p0_line = simd_float3(faceAxes.worldPosition)
             let p1_line = simd_float3(lookAtPointNode.worldPosition)
             let p0_plane = simd_float3(0,0,0)
             let normal_plane = simd_float3(0,0,1)
             
-            
+        
             projectedNode.position = SCNVector3(linePlane(p0_line: p0_line , p1_line: p1_line, p0_plane: p0_plane, normal_plane: normal_plane))
-            //print("projectedNode position: ", projectedNode.position)
             projectedNode.position = SCNVector3(projectedNode.position.x, projectedNode.position.y, -0.05)
             
             
             touchedNode.position = CGPoint(x: Double(self.touchPosition.x), y: Double(self.touchPosition.y))
-            /*pointSKNode.position = convertARKitPositionToSpriteKitPoint(worldPosition: SIMD3(projectedNode.position))*/
-            
-            /*pointSKNode.position = frame.camera.projectPoint(simd_float3(-projectedNode.simdPosition.x, projectedNode.simdPosition.y, projectedNode.simdPosition.z), orientation: .portrait , viewportSize: sceneView.frame.size)
-            pointSKNode.position = CGPoint(x: pointSKNode.position.x , y: pointSKNode.position.y + b/2)*/
+           
             let point = convertARKitPositionToSpriteKitPoint(worldPosition: simd_float3(projectedNode.worldPosition))
             
             if(run == true){
@@ -252,22 +216,14 @@ class ViewController: UIViewController, ARSessionDelegate {
                 distToScreenData.append(DistanceData(timestamp: formattedDifference, Xface:faceAxes.position.x, Yface:faceAxes.position.y, Zface:-faceAxes.position.z, Xdiff: abs(point.x - touchedNode.position.x), Ydiff: abs(point.y - touchedNode.position.y)))
                 
                 pts.append(point)
-                touchPositions.append(touchedNode.position)
                 
                 let k = min(10, pts.count)
                 pointSKNode.position = averageOfLastKCGPoints(array: pts, k: k)!
             }else{
                 pointSKNode.position = point
             }
-            
-            //print("pointSKNode position : ", pointSKNode.position)
-            
-            //print(frame.camera.projectionMatrix.columns.2)
-            //print(frame.camera.viewMatrix(for: .landscapeLeft))
-            //print("camera projection matrix: ", frame.camera.projectionMatrix)
         }
     }
-    
     
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         if anchors.compactMap({
@@ -352,60 +308,11 @@ class ViewController: UIViewController, ARSessionDelegate {
             print(self.pointsPerMeter)
             self.pointsPerMeter = Float(self.a) / self.width
             print(self.pointsPerMeter)
-            //self.pointsPerMeter = Float(self.a + self.b) / (self.width + self.length)
-            //print(self.pointsPerMeter)
             self.run = true
         }
         
     }
     
-    func calibration2(){
-        // Separate events by 2 seconds
-        let delayInSeconds: TimeInterval = 3.0
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + delayInSeconds) {
-            self.calibNode.position = (CGPoint(x:0,y:0))
-            self.upperLeft.position = self.sceneView.unprojectPoint(SCNVector3(self.calibNode.position.x, self.calibNode.position.y, 1))
-            print(self.upperLeft.worldPosition)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + (2 * delayInSeconds)) {
-            // Code to be executed after another 2 seconds
-            self.calibNode.position = (CGPoint(x:0,y:self.b))
-            self.upperRight.position = self.sceneView.unprojectPoint(SCNVector3(self.calibNode.position.x, self.calibNode.position.y, 1))
-            print(self.upperRight.worldPosition)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + (3 * delayInSeconds)) {
-            // Code to be executed after another 2 seconds
-            self.calibNode.position = (CGPoint(x:self.a,y:self.b))
-            self.downLeft.position = self.sceneView.unprojectPoint(SCNVector3(self.calibNode.position.x, self.calibNode.position.y, 1))
-            print(self.downLeft.worldPosition)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + (4 * delayInSeconds)) {
-            self.length = self.upperLeft.position.x - self.upperRight.position.x
-            print(self.length)
-            self.width = self.downLeft.position.y - self.upperRight.position.y
-            print(self.width)
-            self.pointsPerMeter = Float(self.b) / self.length
-            print(self.pointsPerMeter)
-            self.run = true
-        }
-        
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            self.touchPosition = touch.location(in: sceneView.overlaySKScene!)
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            self.touchPosition = touch.location(in: sceneView.overlaySKScene!)
-        }
-    }
     
     func averageOfLastKCGPoints(array: [CGPoint], k: Int) -> CGPoint? {
         // Check if the array has enough elements
